86fedefe9484f3a97be697bfc091d515b9b15e1e
thanhluan.bkhn <thanhluan.bkhn@gmail.com>
2025-01-08 00:14:57 +0700

    update 2025-01-08 00:14:57

--- src/x-modules/mini-app/services/report.service.ts
+++ src/x-modules/mini-app/services/report.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { I18nService } from 'nestjs-i18n';
+import { In } from 'typeorm';
 import { BindRepo } from '~/@core/decorator';
 import { BusinessException } from '~/@systems/exceptions';
 import { I18nTranslations } from '~/assets/i18n.generated';
+import { NSAccount } from '~/common/enums';
 import { UUIDReq } from '~/dto/common.dto';
 import { CreateReportReq, ListReportReq } from '~/dto/signal.dto';
 import { ReportRepo } from '~/repositories/primary';
@@ -23,8 +25,20 @@ export class ReportService {
   }
 
   list(params: ListReportReq) {
+    const listTypes = [NSAccount.EType.FREE];
+
+    if (params?.accountType === NSAccount.EType.PAID_200) {
+      listTypes.push(NSAccount.EType.PAID_200);
+    }
+
+    if (params?.accountType === NSAccount.EType.PAID_2000) {
+      listTypes.push(NSAccount.EType.PAID_200, NSAccount.EType.PAID_2000);
+    }
     return this.reportRepo.findPagination(
       {
+        where: {
+          accountType: In(listTypes),
+        },
         order: {
           createdDate: 'DESC',
         },
